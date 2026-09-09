d003d6648422f916157c3b3ca7b52b10f9321bcc
Musa-Stark <musa.fullstack08@gmail.com>
2026-09-09 14:48:32 +0500

    fix: hint: Configure "jwtSecret" in StarkForge({})

--- src/js/utils/handleJWT.ts
+++ src/js/utils/handleJWT.ts
@@ -32,7 +32,7 @@ export const signJWT = ({
         message: "JWT secret is not configured",
         code: "JWT_SECRET_MISSING",
         statusCode: 500,
-        hint: "Configure 'jwtSecret' in the environment variables.",
+        hint: "Configure 'jwtSecret' in StarkForge({})",
         details: getErrorDetail(routeObj),
       });
     }
@@ -89,7 +89,7 @@ export const verifyJWT = ({
         message: "JWT secret is not configured",
         code: "JWT_SECRET_MISSING",
         statusCode: 500,
-        hint: "Configure 'jwtSecret' in the environment variables.",
+        hint: "Configure 'jwtSecret' in StarkForge({})",
         details: getErrorDetail(routeObj),
       });
 
